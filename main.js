e703158aa395a5f82f55be0e1d7f895afd5ba33c
Dev <annablackwell44@gmail.com>
2016-01-30 21:27:13 +1300

    Working technicolour boxes

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 //when mouse enters a box
-  var newColour = document.getElementsByClassName("box").addEventListener("mouseenter", function(){
-    this.style.backgroundColor = randomColour
-  });
+var boxes = document.getElementsByClassName("box")
+console.log(boxes);
+
+for (i = 0; i < boxes.length; i++) {
+  console.log("adding onmouseover to box " + i);
+  boxes[i].onmouseover = function() {
+    console.log("In a box");
+    this.style.backgroundColor = randomColour()
+  };
+}
 
 //change CSS background colour of that box to a random hexadecimal
 var randomColour = function(){
   var newColour = "#";
   for (i=0; i<3; i++) {
     newColour += ("0" + (Math.random()*265|0).toString(16)).substr(-2);   
-  }}
+  }
+  return newColour;
+}
+
+  console.log(randomColour())
