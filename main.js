f0fdf792b6b4d8028b80a1c2093adb7d461f6c98
Dev <annablackwell44@gmail.com>
2016-01-30 13:20:44 +1300

    First attempt at technicolour javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+//when mouse enters a box
+  var newColour = document.getElementsByClass("box").addEventListener("mouseenter", function(){
+    this.style.backgroundColor = randomColour
+  });
+
+//change CSS background colour of that box to a random hexadecimal
+var randomColour = function(){
+  var newColour = "#";
+  for (i=0; i<3; i++) {
+    newColour += ("0" + (Math.random()*265|0).toString(16)).substr(-2);   
+  }}
